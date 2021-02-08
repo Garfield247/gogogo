4a56c183ebf13a0c86ea93ff18a83ce02ecc9e68
CatMan <garfield_lv@163.com>
2021-02-08 14:20:49 +0800

    fix(func): 添加多返回值的另一种方式

--- go_basic/14-func/03-func.go
+++ go_basic/14-func/03-func.go
@@ -8,10 +8,24 @@ func main() {
 	fmt.Printf("count=%d,sum=%d\n", count, num)
 }
 
+// 返回值声明名称及类型
 func sum(arg ...int) (count int, num int) {
 	fmt.Printf("args:%v", arg)
 	count = len(arg)
 	num = 0
+
+	for _, n := range arg {
+		num += n
+	}
+
+	return count, num
+}
+
+//返回值只声明类型
+func sum2(arg ...int) (int, int) {
+	fmt.Printf("args:%v", arg)
+	count := len(arg)
+	num := 0
 
 	for _, n := range arg {
 		num += n
