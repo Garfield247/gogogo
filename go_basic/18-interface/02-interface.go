8e2c5b98ab5a12ce72cf1dbdb5042c46ff31758e
CatMan <garfield_lv@163.com>
2021-02-10 17:40:19 +0800

    feat(interface): 空接口的应用

--- go_basic/18-interface/02-interface.go
+++ go_basic/18-interface/02-interface.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	//所有的类型都实现了空接口,所以空接口就储存了任意的数据类型
 	var a1 A = Cat{color: "black"}
 	var a2 A = Person{name: "xiaoming", age: 3}
 	var a3 A = "hello interface"
@@ -13,11 +14,21 @@ func main() {
 	fmt.Println(a3)
 	fmt.Println(a4)
 
+	// 使用空接口定义可以存储任意类型的map
+	m1 := make(map[string]interface{})
+	m1["name"] = "map"
+	m1["age"] = 1
+	fmt.Println(m1)
+
+	// 使用空接口定义可以存储任意类型的切片
+	s1 := make([]interface{}, 0, 10)
+	s1 = append(s1, a1, a2, a3, a4, m1)
+	fmt.Println(s1)
+
 }
 
 type A interface {
 	//定义一个接口 不包含任何的方法
-	//所有的类型都实现了空接口,所以空接口就储存了任意的数据类型
 }
 
 type Cat struct {
@@ -27,4 +38,9 @@ type Cat struct {
 type Person struct {
 	name string
 	age  int
+}
+
+// 将空接口作为函数的参数
+func printObj(a interface{}) {
+	fmt.Sprintln(a)
 }
