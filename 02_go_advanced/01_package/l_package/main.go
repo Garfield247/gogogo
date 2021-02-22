d39241c518531231c02102adc6e30c3ccda87362
CatMan <garfield_lv@163.com>
2021-02-22 09:54:48 +0800

    feat(import): _

--- 02_go_advanced/01_package/l_package/main.go
+++ 02_go_advanced/01_package/l_package/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"fmt"
 	"l_package/person"
+	_ "l_package/pkg1" // 使用空白标识符可以只导包初始化且不调用 的情况下不报错
 	"l_package/utils"
 	tu "l_package/utils/timeutils" // 给包起别名 类似 Python  import as
 )
---
+++ 02_go_advanced/01_package/l_package/pkg1/pk1.go
@@ -0,0 +1,8 @@
+// Package pkg1 provides ...
+package pkg1
+
+import "fmt"
+
+func init() {
+	fmt.Println("pkg1 init")
+}
